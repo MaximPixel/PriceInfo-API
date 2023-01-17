afe7db4f1b291a2c1ceee0656f76f70cbcceadb7
MaximPixel <maximpixel@yandex.ru>
2023-01-17 16:27:51 +0200

    Prods response methods

--- src/ProdsResponse.php
+++ src/ProdsResponse.php
@@ -16,6 +16,28 @@ class ProdsResponse implements JsonSerializable {
         $this->data[] = $prodData;
     }
 
+    public function getLastId() {
+        if (isset($this->data[0])) {
+            $lastId = $this->$data[0]->getSkuId;
+
+            foreach ($this->data as $prod) {
+                $id = $prod->getSkuId();
+
+                if ($lastId < $id) {
+                    $lastId = $id;
+                }
+            }
+
+            return $lastId;
+        }
+
+        return null;
+    }
+
+    public function count() {
+        return count($this->data);
+    }
+
     public function jsonSerialize() {
         return $this->data;
     }
